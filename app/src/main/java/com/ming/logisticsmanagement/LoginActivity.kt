2290692df99b8cc268ac3560a931e28b4b35c399
MingEli <ming_eli@163.com>
2021-05-15 22:21:24 +0800

    Splash界面View层实现

---
+++ app/src/main/java/com/ming/logisticsmanagement/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.ming.logisticsmanagement
+
+class LoginActivity:BaseActivity() {
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_login
+    }
+}
--- app/src/main/java/com/ming/logisticsmanagement/SplashActivity.kt
+++ app/src/main/java/com/ming/logisticsmanagement/SplashActivity.kt
@@ -1,7 +1,35 @@
 package com.ming.logisticsmanagement
 
-class SplashActivity : BaseActivity() {
+import android.content.Intent
+import android.os.Handler
+import com.ming.logisticsmanagement.contract.SplashContract
+
+class SplashActivity : BaseActivity() , SplashContract.View {
+
+    companion object{
+        val DELAY = 2000L
+    }
+
+    val handler by lazy {
+        Handler()
+    }
+
     override fun getLayoutResId(): Int {
         return R.layout.activity_splash
+    }
+
+
+
+    //延时2s，跳转到登录页面
+    override fun onNotLoggedIn() {
+        handler.postDelayed({
+            startActivity(Intent().setClass(this@SplashActivity,LoginActivity::class.java))
+            finish()
+        },DELAY)
+    }
+
+    //跳转到主界面
+    override fun onLoggedIn() {
+        startActivity(Intent().setClass(this@SplashActivity,MainActivity::class.java))
     }
 }
