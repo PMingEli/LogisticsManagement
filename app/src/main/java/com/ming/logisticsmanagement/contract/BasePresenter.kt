2027a6043599c5a323324fbfcb8564e7b3539ecd
MingEli <ming_eli@163.com>
2021-05-15 21:57:23 +0800

    Splash界面MVP协议

---
+++ app/src/main/java/com/ming/logisticsmanagement/contract/BasePresenter.kt
@@ -0,0 +1,5 @@
+package com.ming.logisticsmanagement.contract
+
+interface BasePresenter {
+
+}
---
+++ app/src/main/java/com/ming/logisticsmanagement/contract/SplashContract.kt
@@ -0,0 +1,11 @@
+package com.ming.logisticsmanagement.contract
+
+interface SplashContract {
+    interface Presenter:BasePresenter{
+        fun checkLoginStatus()//检查登录状态
+    }
+    interface View{
+        fun onNotLoggedIn()//没有登录的ui处理
+        fun onLoggedIn()//已经登录的ui处理
+    }
+}
