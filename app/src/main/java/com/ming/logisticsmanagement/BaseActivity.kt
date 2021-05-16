75989d83075fbe3a6aa90e65875559a07ced49a0
MingEli <ming_eli@163.com>
2021-05-16 11:36:06 +0800

    登录界面View层实现

--- app/src/main/java/com/ming/logisticsmanagement/BaseActivity.kt
+++ app/src/main/java/com/ming/logisticsmanagement/BaseActivity.kt
@@ -1,9 +1,15 @@
 package com.ming.logisticsmanagement
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    val progressDialog by lazy {
+        ProgressDialog(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutResId())
@@ -17,4 +23,13 @@ abstract class BaseActivity : AppCompatActivity() {
 
     //子类必须实现方法返回一个布局资源的id
     abstract fun getLayoutResId(): Int
+
+    fun showProgress(message:String){
+        progressDialog.setMessage(message)
+        progressDialog.show()
+    }
+
+    fun dismissProgress(){
+        progressDialog.dismiss()
+    }
 }
--- app/src/main/java/com/ming/logisticsmanagement/LoginActivity.kt
+++ app/src/main/java/com/ming/logisticsmanagement/LoginActivity.kt
@@ -1,8 +1,41 @@
 package com.ming.logisticsmanagement
 
-import android.os.Bundle
+import com.ming.logisticsmanagement.contract.LoginContract
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import java.lang.*
 
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(),LoginContract.View {
     override fun getLayoutResId(): Int = R.layout.activity_login
+
+    override fun onUserNameError() {
+        userName.error = getString(R.string.user_name_error)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onStartLogin() {
+        //弹出进度条
+        showProgress(getString(R.string.logging))
+    }
+
+    override fun onLoggedInSuccess() {
+        //隐藏进度条
+        dismissProgress()
+        //进入主界面
+        startActivity<MainActivity>()
+        //退出LoginActivity
+        finish()
+    }
+
+    override fun onLoggedInFailed() {
+        //隐藏进度条
+        dismissProgress()
+        //弹出Toast
+        toast(R.string.login_failed)
+    }
 
 }
